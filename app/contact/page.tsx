a7582f5d0278674223bbf1c27d2951a69f65c2b0
Hiroshi Yamamoto <digiangler777@gmail.com>
2024-10-19 11:20:10 +0900

    お問い合わせフォームをリファクタリングしてテキストコンテンツを更新

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -48,14 +48,13 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* TODO: フォームの項目を修正 */}
+          {/* REVIEW: フォームの項目を修正 */}
           {/* NOTE: form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+              <h3 className="text-4xl text-accent">一緒にお仕事しませんか？</h3>
               <p className="text-white/60">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
-                sed impedit totam quia fugit!
+                お客様とは、内臓と内臓を擦れ合う関係を築いていければと思っております。
               </p>
               {/* NOTE: input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
